50062c22a963b7b616a332ff45f0d49bedd211e1
Luis Mastrangelo <luis@swirldslabs.com>
2024-08-06 01:34:10 +0200

    Fix `vm.rpc` test by upgrading Foundry

--- test/HTSRPC.t.sol
+++ test/HTSRPC.t.sol
@@ -9,10 +9,19 @@ contract HTSRPCTest is Test {
      */
     address USDC_testnet = 0x0000000000000000000000000000000000068cDa;
 
-    function test_RPCETH_call_not_working() external {
+    /**
+     * Related Foundry issue https://github.com/foundry-rs/foundry/issues/8287.
+     *
+     * Update Foundry with `foundryup` if this test is not working.
+     */
+    function test_RPCETH_USDC_decimals() external {
         // $ cast sig 'decimals()'
         // 0x313ce567
         bytes memory result = vm.rpc("eth_call", string.concat('[{"to":"', vm.toString(USDC_testnet), '", "data":"', vm.toString(bytes(hex"313ce567")),'"}, ', '"latest"]'));
         console.logBytes(result);
+
+        // Reinterpret `bytes` results as `uint256`
+        uint256 decimals = uint256(bytes32(result));
+        assertEq(decimals, 6);
     }
 }
