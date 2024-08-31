14f6e3226313ab231c15e011158c0ba524e2cfb0
Luis Mastrangelo <luis@swirldslabs.com>
2024-08-31 02:35:32 +0200

    Improve NatSpec docs
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- src/IERC20.sol
+++ src/IERC20.sol
@@ -2,18 +2,21 @@
 pragma solidity ^0.8.0;
 
 /**
+ * These events should be emitted by `transfer|transferFrom` and `approve` respectively.
  *
+ * See https://ethereum.org/en/developers/docs/standards/tokens/erc-20/#events for more information.
  */
 interface IERC20Events {
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
 }
 
 /**
+ * No need to inherit `IERC20Events` here.
+ * This interface is used to get the selectors and for testing.
  *
  * https://hips.hedera.com/hip/hip-218
  * https://hips.hedera.com/hip/hip-376
- * no need to inherit events here
  */
 interface IERC20 {
     function name() external view returns (string memory);
