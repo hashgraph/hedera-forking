a952ab76ff1f1f14746901fa20c8065898985e7d
Luis Mastrangelo <luis@swirldslabs.com>
2024-09-01 20:26:03 +0200

    Split storage offset `expect`s
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- test/storageLayout.test.js
+++ test/storageLayout.test.js
@@ -3,11 +3,6 @@ const { expect } = require('chai');
 const { storageLayout } = require('../out/HtsSystemContract.sol/HtsSystemContract.json');
 
 describe('::storageLayout', function () {
-    it('should have all slots at `offset` `0` (to avoid packing, thus making many requests per slot)', function () {
-        for (const slot of storageLayout.storage) {
-            expect(slot.offset, slot.label).to.be.equal(0);
-        }
-    });
 
     it('should have one slot per field (sanity check to ensure slots are unique)', function () {
         const set = new Set(storageLayout.storage.map(slot => Number(slot.slot)));
@@ -16,5 +11,13 @@ describe('::storageLayout', function () {
 
     it('should contain only string and number slot types (to ensure all types are supported)', function () {
         expect(storageLayout.types).to.have.all.keys('t_uint8', 't_uint256', 't_string_storage');
+    });
+
+    describe('storage', function () {
+        storageLayout.storage.forEach(({label, offset, slot, type}) => {
+            it(`should have slot \`${label}(${slot}): ${type}\` at \`offset\` \`0\` (to avoid packing, thus avoiding making many requests per slot)`, function () {
+                expect(offset, label).to.be.equal(0);
+            });
+        });
     });
 });
