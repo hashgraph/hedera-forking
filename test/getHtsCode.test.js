21a8714b0217a58b4bf28fe5ea34c7b3c33e4b03
Luis Mastrangelo <acuarica@users.noreply.github.com>
2024-09-03 17:26:39 +0200

    test: include test check to validate `storageLayout` used types (#26)
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- test/getHtsCode.test.js
+++ test/getHtsCode.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 
 const { getHtsCode } = require('@hashgraph/hedera-forking');
 
-describe('getHtsCode', function () {
+describe('::getHtsCode', function () {
 
     it('should be larger than `0xfe` (invalid instruction bytecode)', function () {
         const bytecode = getHtsCode();
