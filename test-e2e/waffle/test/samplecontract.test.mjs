bfe76bd99057b30fa9823ec107914348e5d22885
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2024-09-25 12:14:51 +0200

    feat: adding jsdocs to the samplecontract tests.
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- test-e2e/waffle/test/samplecontract.test.mjs
+++ test-e2e/waffle/test/samplecontract.test.mjs
@@ -1,61 +1,69 @@
-/*-
- *
- * Test e2e: Hedera Waffle Project
- *
- * Copyright (C) 2024 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-import { expect, use } from 'chai';
+import { expect } from 'chai';
 import dotenv from 'dotenv';
-import { providers, Wallet, ContractFactory, ethers } from 'ethers';
+import { providers, Wallet, ContractFactory, Contract } from 'ethers';
 import { createFixtureLoader } from 'ethereum-waffle';
-
 import IERC20Contract from '../build/IERC20.json' assert { type: "json" };
 
 dotenv.config();
 const { JsonRpcProvider } = providers;
 
-const usdcAddress = process.env.ERC20_TOKEN_ADDRESS;
+const usdcAddress = process.env.ERC20_TOKEN_ADDRESS || '';
 const bob = '0x0000000000000000000000000000000000000887';
 
 describe('RPC', () => {
+    /**
+     * @type {function(any): Promise<Contract>}
+     */
     let loadFixture;
+
+    /**
+     * @type {function(Wallet[]): Contract}
+     */
     let fixture;
+
+    /**
+     * @type {Wallet}
+     */
     let wallet;
 
     beforeEach(async () => {
+        /**
+         * @type {providers.JsonRpcProvider}
+         */
         const provider = new JsonRpcProvider(process.env.RELAY_ENDPOINT);
+
         wallet = new Wallet(process.env.OPERATOR_PRIVATE_KEY, provider);
+
+        // Assign the fixture loader
         loadFixture = createFixtureLoader([wallet]);
+
+        // The fixture returns a Contract instance for IERC20
         fixture = ([wallet]) => ContractFactory.getContract(usdcAddress, IERC20Contract.abi, wallet);
     });
 
     it('should have initial balance', async () => {
+        /**
+         * @type {Contract}
+         */
         const contract = await loadFixture(fixture);
-        expect((await contract.balanceOf(wallet.address)).toNumber()).to.not.equal(
-          0,
-          `Please use an account with a non-zero ${usdcAddress} token balance for this test to function correctly.`
-        );
-        const currentBalance = await contract.balanceOf(wallet.address);
-        await contract.transfer(bob, currentBalance.toNumber()); // We transfer everything out.
+
+        const balance = (await contract.balanceOf(wallet.address)).toNumber();
+
+        expect(balance).to.not.equal(0, `Please use an account with a non-zero ${usdcAddress} token balance for this test to function correctly.`);
+
+        const currentBalance = (await contract.balanceOf(wallet.address)).toNumber();
+        await contract.transfer(bob, currentBalance); // We transfer everything out.
         expect((await contract.balanceOf(wallet.address)).toNumber()).to.equal(0);
     });
 
     it('should have some initial balance again!', async () => {
+        /**
+         * @type {Contract}
+         */
         const contract = await loadFixture(fixture);
-        expect((await contract.balanceOf(wallet.address)).toNumber()).to.not.equal(0);
+
+        const balance = (await contract.balanceOf(wallet.address)).toNumber();
+
+        expect(balance).to.not.equal(0);
     });
 });
