2ff6ea20bfe09efbd08c2ba19f4d006a87a8642a
Luis Mastrangelo <luis@swirldslabs.com>
2024-10-02 22:56:56 +0200

    Test projects with different `hardhat.config.js` files
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

---
+++ @hardhat-forking-plugin/test/project.test.js
@@ -0,0 +1,49 @@
+/*-
+ * Hedera Hardhat Forking Plugin
+ *
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const { expect } = require('chai');
+const { resetHardhatContext } = require('hardhat/plugins-testing');
+const path = require('path');
+
+describe('Hardhat Runtime Environment extension', function () {
+    this.timeout(30000);
+
+    [
+        'hardhat-project',
+        'non-hedera-project',
+    ].forEach(project => {
+        describe(`project \`${project}\``, function () {
+
+            /**@type {import('hardhat/types').HardhatRuntimeEnvironment} */
+            let hre;
+            beforeEach('loading hardhat environment', function () {
+                process.chdir(path.join(__dirname, project));
+                hre = require('hardhat');
+            });
+
+            afterEach('resetting hardhat', function () {
+                resetHardhatContext();
+            });
+
+            it('hardhat test', async function () {
+                const exitCode = await hre.run('test');
+                expect(exitCode).to.be.equal(0, 'hardhat test failed');
+            });
+        });
+    });
+});
