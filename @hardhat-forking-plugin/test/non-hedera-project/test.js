18cf997913e394c32972bc6603f843bca1550465
Luis Mastrangelo <luis@swirldslabs.com>
2024-10-11 02:28:05 +0200

    Improve tests
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- @hardhat-forking-plugin/test/non-hedera-project/test.js
+++ @hardhat-forking-plugin/test/non-hedera-project/test.js
@@ -21,6 +21,14 @@ const { expect } = require('chai');
 
 describe('non-hedera-project', function () {
 
+    it('should leave `forking` config untouched', async function () {
+        expect(hre.config.networks.hardhat.forking).to.be.undefined;
+    });
+
+    it('should not activate JSON-RPC Forwarder', async function () {
+        expect(hre.jsonRPCForwarder).to.be.undefined;
+    });
+
     it('should load Hedera hardfork history even on non-Hedera projects', async function () {
         const { chains } = /** @type{import('hardhat/types').HardhatNetworkConfig} */(hre.network.config);
         expect(chains.get(295)).to.be.deep.equal({
