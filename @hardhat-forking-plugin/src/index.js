1fc32e6bf0601599445c0ddde00e246e23a8c5aa
Luis Mastrangelo <luis@swirldslabs.com>
2024-10-11 02:23:18 +0200

    Improve HTS emulation activation
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- @hardhat-forking-plugin/src/index.js
+++ @hardhat-forking-plugin/src/index.js
@@ -17,9 +17,9 @@
  */
 
 const path = require('path');
-const log = require('util').debuglog('hedera-forking');
+const debug = require('util').debuglog('hedera-forking');
 const { Worker } = require('worker_threads');
-const { extendConfig } = require('hardhat/config');
+const { extendConfig, extendEnvironment } = require('hardhat/config');
 
 const chains = {
     295: 'https://mainnet-public.mirrornode.hedera.com/api/v1/',
@@ -52,19 +52,64 @@ const chains = {
  * https://hardhat.org/hardhat-network/docs/guides/forking-other-networks#using-a-custom-hardfork-history
  * https://hardhat.org/hardhat-network/docs/reference#chains
  * 
- * ### Activation of HTS emulation
+ * ### Configuration of HTS emulation
  * 
+ * When `chainId` is present in the `forking` property, we try to activate HTS emulation.
+ * In this step we only setup the config.
+ * See `extendEnvironment` for its actual activation.
+ */
+extendConfig((config, userConfig) => {
+    debug('extendConfig');
+
+    const hardhatChains = config.networks.hardhat.chains;
+    for (const chainIdKey of Object.keys(chains)) {
+        const chainId = Number(chainIdKey);
+        // This can be already set if the user configures a custom hardfork for a Hedera network.
+        // We don't want to overwrite the value set by the user.
+        if (hardhatChains.get(chainId) === undefined) {
+            debug(`Setting hardfork history for chain ${chainId}`);
+            hardhatChains.set(Number(chainId), {
+                hardforkHistory: new Map().set('shanghai', 0)
+            });
+        } else {
+            debug(`Hardfork history for chain ${chainId} set by the user`);
+        }
+    }
+
+    const forking = userConfig.networks?.hardhat?.forking;
+    if (forking !== undefined && 'chainId' in forking) {
+        const chainId = Number(forking.chainId);
+        const mirrorNodeUrl = chains[/**@type{keyof typeof chains}*/(chainId)];
+        if (mirrorNodeUrl !== undefined) {
+            const workerPort = 'workerPort' in forking
+                ? Number(forking.workerPort)
+                : 1234;
+            const forkingConfig = config.networks.hardhat.forking;
+            // Should always be true
+            if (forkingConfig !== undefined) {
+                forkingConfig.chainId = chainId;
+                forkingConfig.mirrorNodeUrl = mirrorNodeUrl;
+                forkingConfig.workerPort = workerPort;
+                debug(`HTS emulation configured:`, forkingConfig);
+            }
+        } else {
+            debug(`Mirror Node URL for chainId ${chainId} not found, HTS emulation disabled`);
+        }
+    }
+});
+
+/**
  * It creates a service `Worker` that intercepts the JSON-RPC calls made to the remote network.
  * This is called directly by EDR,
  * so we can hook into `eth_getCode` and `eth_getStorageAt` to provide HTS emulation.
  * 
- * This needs to be done in the `extendConfig` because it changes the `forking.url`.
+ * This needs to be done in the `extendEnvironment` (or `extendConfig`) because it changes the `forking.url`.
  * The `forking.url` is then used to create an `EdrProviderWrapper` instance.
  * The [`extendProvider`](https://hardhat.org/hardhat-runner/docs/advanced/building-plugins#extending-the-hardhat-provider)
  * hook runs after the `EdrProviderWrapper` has been constructed,
  * and it cannot be changed directly from the `EdrProviderWrapper` instance.
  * 
- * This creates a problem because the `extendConfig` callback cannot be `async`.
+ * This creates a problem because the `extendEnvironment` callback cannot be `async`.
  * Essentially the main issue is "since Hardhat needs to be loadable via `require` call, configuration must be synchronous".
  * See the following issues for more details
  * 
@@ -72,51 +117,39 @@ const chains = {
  * - https://github.com/NomicFoundation/hardhat/issues/2496
  * 
  * That's why we need to shift the setting of `chainId` and `workerPort` to the user,
- * so we can update the `forking.url` if needed synchronously.
+ * so we can update the `forking.url` if needed **synchronously**.
  */
-extendConfig((config, userConfig) => {
-    const hardhatChains = config.networks.hardhat.chains;
-    for (const chainIdKey of Object.keys(chains)) {
-        const chainId = Number(chainIdKey);
-        // This can be already set if the user configures a custom hardfork for a Hedera network.
-        // We don't want to overwrite the value set by the user.
-        if (hardhatChains.get(chainId) === undefined) {
-            log('Setting hardfork history for chain %d', chainId);
-            hardhatChains.set(Number(chainId), {
-                hardforkHistory: new Map().set('shanghai', 0)
-            });
-        } else {
-            log(`Hardfork history for chain %d set by the user`, chainId);
-        }
-    }
+extendEnvironment(hre => {
+    debug('extendEnvironment network config:', hre.network.config);
 
-    const forking = userConfig.networks?.hardhat?.forking;
-    if (forking !== undefined && 'chainId' in forking) {
-        // @ts-ignore
-        let { chainId, workerPort } = forking;
-        if (workerPort === undefined) {
-            workerPort = 1234;
-        }
-        const mirrorNodeUrl = chains[/**@type{keyof typeof chains}*/(chainId)];
-        log(`Forking enabled using chainId=${chainId} workerPort=${workerPort}`);
-        if (mirrorNodeUrl !== undefined) {
+    if ('forking' in hre.network.config) {
+        const forking = hre.network.config.forking;
+        if (forking.chainId !== undefined) {
             const scriptPath = path.resolve(__dirname, './json-rpc-forwarder');
-            log('Starting JSON-RPC Forwarder server from `%s`', scriptPath);
+            debug(`Creating JSON-RPC Forwarder server from \`${scriptPath}\``);
             const worker = new Worker(scriptPath, {
                 workerData: {
                     forkingUrl: forking.url,
-                    mirrorNodeUrl,
-                    port: workerPort,
+                    mirrorNodeUrl: forking.mirrorNodeUrl,
+                    port: forking.workerPort,
                 }
             });
             worker.on('error', err => console.log(err));
-            worker.on('exit', code => log('worker exited with code %d', code));
+            worker.on('exit', code => debug(`Worker exited with code ${code}`));
+
+            hre.jsonRPCForwarder = new Promise(
+                resolve => {
+                    worker.on('message', message => {
+                        if (message === 'listening') {
+                            debug(`JSON-RPC Forwarder ${message}...`);
+                            resolve(worker);
+                        }
+                    });
+                }
+            );
             worker.unref();
-            process.on('exit', code => log('Main process exited with code %d', code));
-            // Should always be true
-            if (config.networks.hardhat.forking !== undefined) {
-                config.networks.hardhat.forking.url = `http://127.0.0.1:${workerPort}`;
-            }
+            process.on('exit', code => debug(`Main process exited with code ${code}`));
+            forking.url = `http://127.0.0.1:${forking.workerPort}`;
         }
     }
 });
