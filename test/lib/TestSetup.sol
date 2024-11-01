98392c8611bcc566abe9a205006044f043a138c6
Luis Mastrangelo <luis@swirldslabs.com>
2024-11-02 00:53:01 +0100

    Expand test setup to include HTS with Mirror Node backend
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- test/lib/TestSetup.sol
+++ test/lib/TestSetup.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.17;
 
 import {console} from "forge-std/console.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 import {MocksToStorageLoader} from "./MocksToStorageLoader.sol";
 
-abstract contract TestSetup {
+abstract contract TestSetup is StdCheats {
+    // `vm` is private in StdCheats, so we duplicate it here
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
     address internal constant HTS = 0x0000000000000000000000000000000000000167;
 
     /**
@@ -23,15 +28,22 @@ abstract contract TestSetup {
     MocksToStorageLoader private loader;
 
     /**
-     *
+     * 3 modes
      */
     function setUpMockStorageForNonFork() internal {
         console.log("HTS code has %d bytes", HTS.code.length);
         if (HTS.code.length == 0) {
+            console.log("HTS code length is 0, non-fork test, code and data provided locally");
             loader = new MocksToStorageLoader(HTS);
             loader.loadHts();
             loader.loadToken(USDC, "USDC");
             loader.loadToken(MFCT, "MFCT");
+        } else if (HTS.code.length == 1) {
+            console.log("HTS code length is 1, forking from a remote Hedera network, HTS code with Mirror Node backend is deployed here");
+            deployCodeTo("HtsSystemContractInitialized.sol", HTS);
+            vm.allowCheatcodes(HTS);
+        } else {
+            console.log("HTS code length is greater than 1, HTS code comes from `json-rpc-mock`");
         }
     }
 
