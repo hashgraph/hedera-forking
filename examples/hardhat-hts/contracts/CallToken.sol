d0502e4e28eab5783bf9cf7353a0868b9713eb4e
Luis Mastrangelo <acuarica@users.noreply.github.com>
2024-11-06 06:11:05 +0100

    docs: improve Hardhat plugin example using local path, intermediate contract and `console.log` (#110)
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- examples/hardhat-hts/contracts/CallToken.sol
+++ examples/hardhat-hts/contracts/CallToken.sol
@@ -2,9 +2,17 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from "./IERC20.sol";
+import {console} from "hardhat/console.sol";
 
 contract CallToken {
     function getTokenName(address tokenAddress) external view returns (string memory) {
         return IERC20(tokenAddress).name();
+    }
+
+    function invokeTransferFrom(address tokenAddress, address to, uint256 amount) external {
+        // You can use `console.log` as usual
+        // https://hardhat.org/tutorial/debugging-with-hardhat-network#solidity--console.log
+        console.log("Transferring from %s to %s %s tokens", msg.sender, to, amount);
+        IERC20(tokenAddress).transferFrom(msg.sender, to, amount);
     }
 }
