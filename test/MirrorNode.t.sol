999482981db24d3fb58cc7cd0a1e2f0cc9e19779
Luis Mastrangelo <luis@swirldslabs.com>
2024-11-11 13:51:34 +0100

    Fix mirror node test in CI
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- test/MirrorNode.t.sol
+++ test/MirrorNode.t.sol
@@ -6,21 +6,31 @@ import "../src/MirrorNode.sol" as MirrorNode;
 
 contract MirrorNodeTest is Test {
 
-    address private USDC = 0x0000000000000000000000000000000000068cDa;
+    address private constant USDC = 0x0000000000000000000000000000000000068cDa;
+
+    bool private _skip;
 
     function setUp() external {
-        vm.chainId(296);
+        try vm.activeFork() {
+            _skip = true;
+            console.log("Active fork detected, this test runs only in non-forked mode");
+        } catch {
+            _skip = false;
+            vm.chainId(296);
 
-        string memory PATH = vm.envString("PATH");
-        vm.setEnv("PATH", string.concat("./scripts:", PATH));
+            string memory PATH = vm.envString("PATH");
+            vm.setEnv("PATH", string.concat("./scripts:", PATH));
+        }
     }
 
     function test_get_balance_for_unknown_account() external {
+        vm.skip(_skip);
         uint amount = MirrorNode.getBalance(USDC, makeAddr("user"));
         assertEq(amount, 0);
     }
 
     function test_get_balance_for_existing_account() external {
+        vm.skip(_skip);
         // https://hashscan.io/testnet/account/0.0.1421
         address alice = 0x4D1c823b5f15bE83FDf5adAF137c2a9e0E78fE15;
         uint amount = MirrorNode.getBalance(USDC, alice);
