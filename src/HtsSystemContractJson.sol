29d30ad86eb31e5546979e19823879b4bb17e03f
Victor Yanev <victor.yanev@limechain.tech>
2024-12-06 14:25:02 +0200

    refactor: handle not found error
    Signed-off-by: Victor Yanev <victor.yanev@limechain.tech>

--- src/HtsSystemContractJson.sol
+++ src/HtsSystemContractJson.sol
@@ -62,6 +62,10 @@ contract HtsSystemContractJson is HtsSystemContract {
         }
 
         string memory json = mirrorNode().fetchTokenData(address(this));
+        if (keccak256(bytes(json)) == keccak256(bytes('{"_status":{"messages":[{"message":"Not found"}]}}'))) {
+            // Token not found
+            return;
+        }
 
         assembly { slot := tokenType.slot }
         storeString(address(this), uint256(slot), vm.parseJsonString(json, ".type"));
