5f40fb94d1baa7c15aa556cede2da6e7f21d02a5
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-01-23 10:50:06 +0100

    fix: applying mr suggestions comparing to equals rather than not equals (#191)
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- examples/hardhat-hts/test/nft-ownerof.test.js
+++ examples/hardhat-hts/test/nft-ownerof.test.js
@@ -24,8 +24,9 @@ describe('NFT example -- ownerOf', function () {
     it('should get `ownerOf` account holder', async function () {
         // https://hashscan.io/mainnet/token/0.0.4970613
         const nft = await getContractAt('IERC721', '0x00000000000000000000000000000000004bd875');
-        expect(await nft['ownerOf'](1)).to.not.be.equal(
-            '0x0000000000000000000000000000000000000000'
-        );
+
+        // The assertion below cannot be guaranteed, since we can only query the current owner of the NFT,
+        // Note that the ownership of the NFT may change over time.
+        expect(await nft['ownerOf'](1)).to.be.equal('0x0000000000000000000000000000000000161927');
     });
 });
