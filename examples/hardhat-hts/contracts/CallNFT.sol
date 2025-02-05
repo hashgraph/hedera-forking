8fd3451bbeae2dcd6b00c870804bb118743c33bc
Luis Mastrangelo <luis@swirldslabs.com>
2025-02-05 17:02:24 -0300

    Use exported interfaces and include examples for calling HTS directly
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- examples/hardhat-hts/contracts/CallNFT.sol
+++ examples/hardhat-hts/contracts/CallNFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import {IERC721} from "./IERC721.sol";
+import {IERC721} from "@hashgraph/system-contracts-forking/contracts/IERC721.sol";
 import {console} from "hardhat/console.sol";
 
 contract CallNFT {
