88fd94293b6703e10d3091edac835f09627d251a
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-12 16:23:46 +0100

    fix: linking lib directly inline in the bytecode (#163)
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- contracts/KeyLib.sol
+++ contracts/KeyLib.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import {IHederaTokenService} from "./IHederaTokenService.sol";
 
 library KeyLib {
-    function keyExists(uint keyType, IHederaTokenService.TokenInfo memory tokenInfo) public pure returns (bool) {
+    function keyExists(uint keyType, IHederaTokenService.TokenInfo memory tokenInfo) internal pure returns (bool) {
         for (uint256 i = 0; i < tokenInfo.token.tokenKeys.length; i++) {
             if (tokenInfo.token.tokenKeys[i].keyType == keyType) {
                 IHederaTokenService.KeyValue memory key = tokenInfo.token.tokenKeys[i].key;
