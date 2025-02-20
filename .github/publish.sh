0c572abeb4c7cf395298678a98b3bc8eec036523
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-20 15:42:00 +0100

    test: wip cov
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- .github/publish.sh
+++ .github/publish.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-# Define the coverage output file
-COVERAGE_OUTPUT="coverage.log"
-awk '/Test Summary:/ {flag=1; next} flag' "$COVERAGE_OUTPUT" | tail -n +3 | awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g' > tmp
-
-sed '2s/.*/'"$(sed -n '4p' tmp)"'/g' tmp > output.txt
-echo "" >> output.txt
-awk '/-╯/ {flag=1; next} flag' $COVERAGE_OUTPUT >> output.txt
-rm tmp
+input=$(cat)
+processed_output=$(echo "$input" | awk '/Test Summary:/ {flag=1; next} flag' | tail -n +3 | awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g')
+line4=$(echo "$processed_output" | sed -n '4p')
+modified_output=$(echo "$processed_output" | awk -v line4="$line4" 'NR==2 {$0=line4} 1')
+final_output=$(echo -e "$modified_output\n\n$(echo "$input" | awk '/-╯/ {flag=1; next} flag')")
+curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" \
+  -X POST "https://api.github.com/repos/hedera-forking/issues/$GITHUB_PR_NUMBER/comments" \
+  -d "{\"body\": \"[coverage-comment-id]: <> (Solidity Coverage)"$'\n\n'"## Coverage Report - Solidity Coverage"$'\n\n'"### Coverage: $final_output"}"
