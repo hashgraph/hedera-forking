3180bf78eba7ab94a7a9c9992313a978b49d10a5
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-20 08:51:13 +0100

    test: adding tests for included surl (#150)
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

---
+++ test/Surl.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../contracts/Surl.sol";
+
+contract SurlTest is Test {
+    string constant VALID_URL = "https://testnet.mirrornode.hedera.com/api/v1/accounts/0.0.4436613?transactions=false";
+    string constant NOT_FOUND_URL = "https://testnet.mirrornode.hedera.com/api/v1/accounts/0.0.0";
+
+    bool skip;
+
+    function setUp() external {
+        skip = address(0x167).code.length != 1; // Only run these tests when the FFI is expected to be enabled anyway
+    }
+
+    function testGetRequestSuccess() external {
+        vm.skip(skip);
+        (uint256 status, bytes memory data) = Surl.get(VALID_URL);
+        assertEq(status, 200);
+        assertTrue(bytes(string(data)).length > 0);
+    }
+
+    function testGetRequestNotFound() external {
+        vm.skip(skip);
+        (uint256 status, bytes memory data) = Surl.get(NOT_FOUND_URL);
+        assertEq(status, 404);
+        assertTrue(bytes(string(data)).length > 0);
+    }
+}
