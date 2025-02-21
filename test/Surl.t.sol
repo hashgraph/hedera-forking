4ed7dafd7ba5f54b28ae2f4381a7b9556f2d257e
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-21 09:40:26 +0100

    fix: using modifier as suggested in pr removing redundant import (#150)
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- test/Surl.t.sol
+++ test/Surl.t.sol
@@ -8,21 +8,18 @@ contract SurlTest is Test {
     string constant VALID_URL = "https://testnet.mirrornode.hedera.com/api/v1/accounts/0.0.4436613?transactions=false";
     string constant NOT_FOUND_URL = "https://testnet.mirrornode.hedera.com/api/v1/accounts/9.0.0";
 
-    bool isFFI;
-
-    function setUp() external {
-        isFFI = address(0x167).code.length == 1; // Only run these tests when the FFI is expected to be enabled anyway
+    modifier isFFI() {
+        vm.skip(address(0x167).code.length != 1); // Only run these tests when the FFI is expected to be enabled anyway
+        _;
     }
 
-    function testGetRequestSuccess() external {
-        vm.skip(!isFFI);
+    function testGetRequestSuccess() isFFI external {
         (uint256 status, bytes memory data) = Surl.get(VALID_URL);
         assertEq(status, 200);
         assertTrue(bytes(string(data)).length > 0);
     }
 
-    function testGetRequestNotFound() external {
-        vm.skip(!isFFI);
+    function testGetRequestNotFound() isFFI external {
         (uint256 status, bytes memory data) = Surl.get(NOT_FOUND_URL);
         assertEq(status, 404);
         assertTrue(bytes(string(data)).length > 0);
