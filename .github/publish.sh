20fbc3f3d6a98c06871d0b87397c29f4c5f64469
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-24 17:11:56 +0100

    feat: test
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- .github/publish.sh
+++ .github/publish.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-
 input=$(cat)
-echo "$input"
-echo "\n----\n"
-processed_output=$(echo "$input" | awk '/Test Summary:/ {flag=1; next} flag' | tail -n +3 | awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g')
-echo "$processed_output"
 
-line4=$(echo "$processed_output" | sed -n '4p')
-modified_output=$(echo "$processed_output" | awk -v line4="$line4" 'NR==2 {$0=line4} 1')
-final_output=$(echo -e "$modified_output\n\n$(echo "$input" | awk '/-╯/ {flag=1; next} flag')")
+test_summary=$(echo "$input" | awk '/Test Suite/ {flag=1} flag' |  awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g')
+test_summary_line4=$(echo "$test_summary" | sed -n '4p')
+modified_test_summary=$(echo "$test_summary" | awk -v line4="$test_summary_line4" 'NR==2 {$0=line4} 1')
 
+test_coverage=$(echo "$input" | awk '/\%\ Statements/ {flag=1} flag' |  awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g')
+test_coverage_line4=$(echo "$test_coverage" | sed -n '4p')
+modified_test_coverage=$(echo "$test_coverage" | awk -v line4="$test_coverage_line4" 'NR==2 {$0=line4} 1')
+
+final_output=$(echo -e "$modified_test_summary\n\n$modified_test_coverage")
 comment_json=$(jq -n --arg body "$final_output" '{"body": $body'})
+
 curl -L \
   -X POST \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
