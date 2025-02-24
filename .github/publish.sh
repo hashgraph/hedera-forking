33d4c7cbfbf3179741863b0b1080aec1ed2272a3
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-24 16:35:36 +0100

    feat: test
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- .github/publish.sh
+++ .github/publish.sh
@@ -7,8 +7,10 @@ line4=$(echo "$processed_output" | sed -n '4p')
 modified_output=$(echo "$processed_output" | awk -v line4="$line4" 'NR==2 {$0=line4} 1')
 final_output=$(echo -e "$modified_output\n\n$(echo "$input" | awk '/-╯/ {flag=1; next} flag')")
 echo "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$GITHUB_PR_NUMBER/comments"
+
+comment_json=$(jq -n --arg body "$final_output" '{"body": $body'})
 curl -L \
   -X POST \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$GITHUB_PR_NUMBER/comments" \
-  -d "{\"body\":\"### Coverage: $final_output\"}"
+  -d "$comment_json"
