d014df0946a4b8bff2806c4c23e157375fed49d3
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-24 17:24:23 +0100

    feat: test
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- .github/publish.sh
+++ .github/publish.sh
@@ -4,11 +4,11 @@ input=$(cat)
 
 test_summary=$(echo "$input" | awk '/Test Suite/ {flag=1} flag' |  awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g')
 test_summary_line4=$(echo "$test_summary" | sed -n '4p')
-modified_test_summary=$(echo "$test_summary" | awk -v line4="$test_summary_line4" 'NR==2 {$0=line4} 1')
+modified_test_summary=$(echo "$test_summary" | awk -v line4="$test_summary_line4" 'NR==2 {$0=line4} 1' | awk -v sep="$test_summary_line4" '$0==sep{if(!found){found=1;print;}next;}{print}')
 
 test_coverage=$(echo "$input" | awk '/\%\ Statements/ {flag=1} flag' |  awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g')
 test_coverage_line4=$(echo "$test_coverage" | sed -n '4p')
-modified_test_coverage=$(echo "$test_coverage" | awk -v line4="$test_coverage_line4" 'NR==2 {$0=line4} 1')
+modified_test_coverage=$(echo "$test_coverage" | awk -v line4="$test_coverage_line4" 'NR==2 {$0=line4} 1' | awk -v sep="$test_coverage_line4" '$0==sep{if(!found){found=1;print;}next;}{print}')
 
 final_output=$(echo -e "$modified_test_summary\n\n$modified_test_coverage")
 comment_json=$(jq -n --arg body "$final_output" '{"body": $body'})
