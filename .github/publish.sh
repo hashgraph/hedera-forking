50cf37cb8d69e3c765d972fe8376615494d69a0f
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-24 13:14:25 +0100

    feat: test
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- .github/publish.sh
+++ .github/publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 input=$(cat)
 processed_output=$(echo "$input" | awk '/Test Summary:/ {flag=1; next} flag' | tail -n +3 | awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g')
 line4=$(echo "$processed_output" | sed -n '4p')
@@ -6,4 +8,4 @@ modified_output=$(echo "$processed_output" | awk -v line4="$line4" 'NR==2 {$0=li
 final_output=$(echo -e "$modified_output\n\n$(echo "$input" | awk '/-╯/ {flag=1; next} flag')")
 curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" \
   -X POST "https://api.github.com/repos/hedera-forking/issues/$GITHUB_PR_NUMBER/comments" \
-  -d "{\"body\": \"[coverage-comment-id]: <> (Solidity Coverage)\"## Coverage Report - Solidity Coverage\"### Coverage: $final_output\"}"
+  -d "{\"body\":\"TEST CONTENT\"}"
