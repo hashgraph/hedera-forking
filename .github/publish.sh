a059f603aacd68f836b0e6b9ce6020c60fb46721
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-24 16:44:03 +0100

    feat: test
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- .github/publish.sh
+++ .github/publish.sh
@@ -3,10 +3,11 @@
 
 input=$(cat)
 processed_output=$(echo "$input" | awk '/Test Summary:/ {flag=1; next} flag' | tail -n +3 | awk '!/╰/{print} /╰/ {exit}' | sed 's/+/|/g')
+echo "$processed_output"
+
 line4=$(echo "$processed_output" | sed -n '4p')
 modified_output=$(echo "$processed_output" | awk -v line4="$line4" 'NR==2 {$0=line4} 1')
 final_output=$(echo -e "$modified_output\n\n$(echo "$input" | awk '/-╯/ {flag=1; next} flag')")
-echo "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$GITHUB_PR_NUMBER/comments"
 
 comment_json=$(jq -n --arg body "$final_output" '{"body": $body'})
 curl -L \
