2d36e0999ccd97e629600e24c23f5f3cd8b03533
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-02-24 14:43:09 +0100

    feat: test
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- .github/publish.sh
+++ .github/publish.sh
@@ -6,6 +6,8 @@ processed_output=$(echo "$input" | awk '/Test Summary:/ {flag=1; next} flag' | t
 line4=$(echo "$processed_output" | sed -n '4p')
 modified_output=$(echo "$processed_output" | awk -v line4="$line4" 'NR==2 {$0=line4} 1')
 final_output=$(echo -e "$modified_output\n\n$(echo "$input" | awk '/-╯/ {flag=1; next} flag')")
-curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" \
-  -X POST "https://api.github.com/repos/hedera-forking/issues/$GITHUB_PR_NUMBER/comments" \
+curl -L \
+  -X POST \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  "https://api.github.com/repos/hedera-forking/issues/$GITHUB_PR_NUMBER/comments" \
   -d "{\"body\":\"TEST CONTENT\"}"
