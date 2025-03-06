1de85d382e44170f974cb6de2ff211fecbc88b37
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2025-03-06 13:30:39 +0100

    chore: switch license declarations to spdx (#259)
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,4 @@
-/*-
- * Hedera Hardhat Forking Plugin
- *
- * Copyright (C) 2024 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// SPDX-License-Identifier: Apache-2.0
 
 const globals = require('globals');
 
@@ -59,7 +43,7 @@ module.exports = [
             ],
 
             // https://github.com/nikku/eslint-plugin-license-header?tab=readme-ov-file#eslint-plugin-license-header
-            'license-header/header': ['error', './resources/license.js.header'],
+            'license-header/header': ['error', ['// SPDX-License-Identifier: Apache-2.0']],
 
             // Disable to use dynamically generated tests https://mochajs.org/#dynamically-generating-tests
             // https://github.com/lo1tuma/eslint-plugin-mocha/blob/main/docs/rules/no-setup-in-describe.md
