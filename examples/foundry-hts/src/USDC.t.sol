9f369e9c08d538da1be4ca3b8a3fa4bf767cb17c
Luis Mastrangelo <luis@swirldslabs.com>
2025-03-29 03:26:01 +0100

    Use proper Foundry project for HTS example
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- examples/foundry-hts/src/USDC.t.sol
+++ examples/foundry-hts/src/USDC.t.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
-import {htsSetup} from "hedera-forking/contracts/htsSetup.sol";
-import {IERC20} from "hedera-forking/contracts/IERC20.sol";
-import {IHederaTokenService} from "hedera-forking/contracts/IHederaTokenService.sol";
-import {HederaResponseCodes} from "hedera-forking/contracts/HederaResponseCodes.sol";
+import {htsSetup} from "hedera-forking/htsSetup.sol";
+import {IERC20} from "hedera-forking/IERC20.sol";
+import {IHederaTokenService} from "hedera-forking/IHederaTokenService.sol";
+import {HederaResponseCodes} from "hedera-forking/HederaResponseCodes.sol";
 
 contract USDCExampleTest is Test {
     // https://hashscan.io/mainnet/token/0.0.456858
